b15fb5895865b020afd7628117c733e8d9a9cbd7
Andres Garcia Muñoz <163463179+littlenegrito@users.noreply.github.com>
2024-09-05 22:27:48 -0400

    menu principal, submenus profesor y curso, correccion metodos sobrecargados

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Menu menu = new Menu();
+		menu.iniciar();
+		
+		/*
+		// Crear el profesor para cada curso
+		Profesor profesorMatematica = new Profesor("Dr. Juan Pérez", "12345678-9", "juan.perez@universidad.edu", "Matemáticas");
+		Profesor profesorFisica = new Profesor("Dr. Laura Gómez", "98765432-1", "laura.gomez@universidad.edu", "Física");
+		Profesor profesorProgramacion = new Profesor("Ing. Carlos Ruiz", "45678901-2", "carlos.ruiz@universidad.edu", "Programación");
+
+		// Crear los cursos con los profesores asignados, usando el constructor básico
+		Curso cursoMatematica = new Curso("Matemática", "Curso de matemáticas avanzadas", profesorMatematica);
+		Curso cursoFisica = new Curso("Física", "Curso de física moderna", profesorFisica);
+		Curso cursoProgramacion = new Curso("Programación Avanzada", "Curso avanzado de programación en Java", profesorProgramacion);
+		*/
+	}
+
+}
